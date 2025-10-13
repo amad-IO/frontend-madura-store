8e949ba85861a91b93df1cfd88c3a62391457a5a
istimata <mochistimata@gmail.com>
2025-10-13 12:03:17 +0700

    update halaman login

--- lib/ui/pages/LoginPage.dart
+++ lib/ui/pages/LoginPage.dart
@@ -148,7 +148,7 @@ class _LoginPageState extends State<LoginPage> {
                         Positioned(
                           top: 146, // atur sesuai kebutuhan
                           child: Text(
-                            'Kasir madura',
+                            'Kasir maduraaaa',
                             textAlign: TextAlign.center,
                             style: GoogleFonts.poppins(
                               color: AppTheme.primaryRed,
